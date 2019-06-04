2815f7b798be0b6bfbb9488efc7cd4eeed8358a0
Tshibanda Patrick <tshibandap@gmail.com>
2019-06-04 11:54:44 +0200

    First commit

---
+++ VideoBackground/VideoPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace VideoBackground
+{
+    public class VideoPlayer : View
+    {
+        public static readonly BindableProperty SourceProperty =
+                BindableProperty.Create(nameof(Source), typeof(string), typeof(VideoPlayer), null);
+
+        public string Source
+        {
+            set { SetValue(SourceProperty, value); }
+            get { return (string)GetValue(SourceProperty); }
+        }
+    }
+}
